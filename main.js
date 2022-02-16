6b5e2642190a70dbead8e76b07fe37a5d928f844
h-oop <99036325+h-oop@users.noreply.github.com>
2022-02-16 09:52:17 -0700

    bbbbb

--- main.js
+++ main.js
@@ -3,89 +3,45 @@
 // Event Listener (Mark Quiz)
 document.getElementById('mark-quiz').addEventListener('click', markQuiz);
 
-let score = 0;
 
-
+////////////////////////////////////
 // Event Function
 function markQuiz() {
     // Initialize Score
-    score = 0;
+    let score = 0;
+
     // Mark q's
-    markQuestion1();
-    markQuestion2();
-    markQuestion3();
-    markQuestion4();
+    score += markQuestion(1, "canada");
+    score += markQuestion(2, "cardinal");
+    score += markQuestion(3, "tai lung");
+    score += markQuestion(4, "pickleball");
 
 
     // Display Quiz Results
     document.getElementById('quiz-score').innerHTML = score;
     document.getElementById('quiz-percent').innerHTML = Math.round(score / 4 * 100);
 
-
 }
-
-
 
 
 
 ///////////////////////////////////////////////////////////////
-//define & invoke - local variables - organized
-function markQuestion1() {
-    let answer = document.getElementById('answer1').value;
-    answer = answer.toLowerCase();
+//define & invoke - local variables - organized - efficiency
+// parameters and arguments - pass data int function
+//return values - return data from a function
+function markQuestion(qNum, correctAnswer) {
 
-    let result1El = document.getElementById('result1');
-    if (answer == 'canada') {
-        result1El.innerHTML = 'Correct';
-        result1El.style.color = 'green';
-        score++;
+    let userAnswer = document.getElementById("answer" + qNum).value;
+    userAnswer = userAnswer.toLowerCase();
+ 
+    let result1El = document.getElementById("result" + qNum);
+    if (userAnswer === correctAnswer) {
+       result1El.innerHTML = 'Correct';
+       result1El.style.color = 'green';
+       return 1;
     } else {
-        result1El.innerHTML = 'Incorrect';
-        result1El.style.color = 'red';
-    }
-}
-
-function markQuestion2() {
-    let answer = document.getElementById('answer2').value;
-    answer = answer.toLowerCase();
-
-    let result2El = document.getElementById('result2');
-    if (answer == 'cardinal') {
-        result2El.innerHTML = 'Correct';
-        result2El.style.color = 'green';
-        score++;
-    } else {
-        result2El.innerHTML = 'Incorrect';
-        result2El.style.color = 'red';
-    }
-}
-
-function markQuestion3() {
-    let answer = document.getElementById('answer3').value;
-    answer = answer.toLowerCase();
-
-    let result3El = document.getElementById('result3');
-    if (answer == 'tai lung') {
-        result3El.innerHTML = 'Correct';
-        result3El.style.color = 'green';
-        score++;
-    } else {
-        result3El.innerHTML = 'Incorrect';
-        result3El.style.color = 'red';
-    }
-}
-
-function markQuestion4() {
-    let answer = document.getElementById('answer4').value;
-    answer = answer.toLowerCase();
-
-    let result4El = document.getElementById('result4');
-    if (answer == 'pickleball') {
-        result4El.innerHTML = 'Correct';
-        result4El.style.color = 'green';
-        score++;
-    } else {
-        result4El.innerHTML = 'Incorrect';
-        result4El.style.color = 'red';
+       result1El.innerHTML = 'Incorrect';
+       result1El.style.color = 'red';
+       return 0;
     }
 }
